331018b1bfec948196091d97eeade045de4280df
Jose <jose.sentis1987@gmail.com>
2019-04-09 00:53:56 +0200

    Changes ciry to be coloured

--- 03-city/script.js
+++ 03-city/script.js
@@ -3,7 +3,7 @@ var NBOXES = 100;
 var BOX_SIZE = 80;
 var BOX_HEIGHT = 500;
 var NCOLS = 8;
-var STREETSIZE = 100;
+var STREETSIZE = 40;
 
 var init = function () {
   renderer = new THREE.WebGLRenderer({
@@ -17,7 +17,6 @@ var init = function () {
   renderer.setClearColor(0xffffff, 0);
   renderer.setPixelRatio(window.devicePixelRatio || 1);
   renderer.shadowMap.enabled = true;
-  // renderer.shadowMap.type = THREE.PCFSoftShadowMap; //THREE.BasicShadowMap;
   renderer.shadowMap.type = THREE.BasicShadowMap;
 
   container.appendChild(renderer.domElement);
@@ -31,7 +30,7 @@ var init = function () {
   );
   camera.position.z = 1200;
   camera.position.y = 400;
-  camera.position.x = 300;
+  camera.position.x = 500;
   camera.lookAt(new THREE.Vector3());
   scene.add(camera);
 
@@ -47,9 +46,8 @@ var init = function () {
   var floor = new THREE.Mesh(
     new THREE.PlaneGeometry(4000, 4000),
     new THREE.MeshStandardMaterial({
-      map: texturePlane,
-      side: THREE.DoubleSide,
-      roughness: 0.9
+      color: 0xc5fad9,
+      side: THREE.DoubleSide
     })
   );
 
@@ -60,60 +58,32 @@ var init = function () {
 
   // Adds buildings and streets
   var boxes = [];
-  var streets = [];
   var height;
   var currentZ = 0;
-  var cityWidth = NCOLS * BOX_SIZE + STREETSIZE * (NCOLS - 1);
+  // var cityWidth = NCOLS * BOX_SIZE + STREETSIZE * (NCOLS - 1);
   var cityDepth = NBOXES / NCOLS *  BOX_SIZE + STREETSIZE * (NBOXES / NCOLS - 1);
-
-  var textureBuilding = textureLoader.load('building.jpg');
-  textureBuilding.wrapS = textureBuilding.wrapT = THREE.RepeatWrapping;
-  textureBuilding.anisotropy = 16;
-
-  var textureStreet = textureLoader.load('street.jpg');
-  textureStreet.wrapS = textureStreet.wrapT = THREE.RepeatWrapping;
-  textureStreet.anisotropy = 16;
+  var bufferGeometry;
+  var buildingMaterial = new THREE.MeshStandardMaterial({ color: 0xf77fee });
 
   for (var i = 0; i < NBOXES; i++) {
     height = Math.floor(Math.random() * BOX_HEIGHT) + 100;
 
-    textureBuilding.repeat.set(1, height/BOX_SIZE);
-    boxes[i] = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(BOX_SIZE, height, BOX_SIZE),
-        new THREE.MeshStandardMaterial({
-          map: textureBuilding,
-          metalness: 0.25,
-          roughness: 0.4
-        }));
+    bufferGeometry = new THREE.BoxGeometry(BOX_SIZE, height, BOX_SIZE);
+    bufferGeometry.faces[0].color.setHex(0xdb66e4);
+    bufferGeometry.colorsNeedUpdate = true
 
-    boxes[i].position.x = (i % NCOLS) * (BOX_SIZE + STREETSIZE);
+    boxes[i] = new THREE.Mesh(bufferGeometry, buildingMaterial);
+
+    boxes[i].position.x = (i % NCOLS) * (BOX_SIZE + STREETSIZE * Math.random());
     boxes[i].position.y = height / 2;
     boxes[i].position.z = currentZ - cityDepth / 2;
     boxes[i].castShadow = true;
 
-    streets[i] = new THREE.Mesh(
-      new THREE.PlaneGeometry(STREETSIZE, BOX_SIZE),
-      new THREE.MeshBasicMaterial({
-        map: textureStreet,
-        side: THREE.DoubleSide,
-        roughness: 0.9
-      })
-    );
-
-    streets[i].rotation.x = -Math.PI / 2;
-    streets[i].receiveShadow = true;
-
-    streets[i].position.x =
-    (i % NCOLS) * (BOX_SIZE + STREETSIZE) + BOX_SIZE + (STREETSIZE - BOX_SIZE) / 2;
-    streets[i].position.z = currentZ - cityDepth / 2;
-
     if (i % NCOLS === NCOLS - 1) {
-      streets[i].visible = false;
       currentZ += BOX_SIZE + STREETSIZE;
     }
 
     scene.add(boxes[i]);
-    scene.add(streets[i]);
   }
 
   // --- SETUP LIGHTS ----------------------------------------------------------------
@@ -141,18 +111,16 @@ var init = function () {
 
   // --- INTERACTION STUFF -----------------------------------------------------------
   var controls = new THREE.OrbitControls(camera, container);
-  // controls.autoRotate = true;
-  // controls.autoRotateSpeed = 0.2;
   controls.enableDamping = true;
   controls.minPolarAngle = Math.PI / 4;
   controls.maxPolarAngle = Math.PI * 85 / 180;
-  // controls.enableZoom = false;
-  // controls.enablePan = false;
 };
 
 var loop = function () {
-  requestAnimationFrame(loop);
+  camera.position.z = camera.position.z - 0.5;
+
   renderer.render(scene, camera);
+  requestAnimationFrame(loop);
 };
 
 init();
