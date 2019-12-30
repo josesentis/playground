294d13d877a0fd595c5822d0ace88320e3fea801
Jose <jose@runroom.com>
2019-12-30 17:37:24 +0100

    Adds src files from codrops example

---
+++ 05-glitch/js/script.js
@@ -0,0 +1,18 @@
+(function() {
+    const container = document.body
+    const itemsWrapper = document.querySelector('.grid')
+
+    // Preload images
+    const preloadImages = () => {
+        return new Promise((resolve, reject) => {
+            imagesLoaded(document.querySelectorAll('img'), resolve);
+        });
+    };
+
+    // And then..
+    preloadImages().then(() => {
+        // Remove the loader
+        document.body.classList.remove('loading');
+        const effect = new RGBShiftEffect(container, itemsWrapper, { strength: 0.25 })
+    });
+})();
