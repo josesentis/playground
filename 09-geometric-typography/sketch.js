9398ad4fd38c827f441d98d06f633456dd2ff2a7
Jose <jose@runroom.com>
2020-03-22 12:54:02 +0100

    Adds geometric typography exercice

---
+++ 09-geometric-typography/sketch.js
@@ -0,0 +1,31 @@
+const circleSize = 475;
+const canvasSize = 525;
+const backgroundColor = '#191919';
+const mainColor = '#f1f1f1';
+let iteration = 4;
+let increase = true;
+
+function setup() {
+  createCanvas(canvasSize, canvasSize);
+  background(254);
+}
+
+function draw() {
+  const strokeSize = circleSize / iteration;
+  iteration = iteration + (increase ? +1 : -1);
+
+  if (iteration === 50 || iteration === 0) increase = !increase;
+
+  background(backgroundColor);
+  noFill();
+  stroke(mainColor);
+  strokeWeight(strokeSize);
+  ellipse(width / 2, height / 2, circleSize);
+  noStroke();
+  fill(backgroundColor);
+  triangle(width / 2, height / 2, canvasSize, 150, canvasSize, canvasSize - 150);
+  fill(mainColor);
+  stroke(mainColor);
+  strokeWeight(strokeSize);
+  line(width / 2, height / 2, canvasSize, canvasSize);
+}
