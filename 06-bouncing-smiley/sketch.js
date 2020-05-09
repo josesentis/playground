bddd3440170a4eace89dd0c5b3c26d1f04281d15
Jose <jose@runroom.com>
2020-05-10 00:43:25 +0200

    Adds reset game button

--- 06-bouncing-smiley/sketch.js
+++ 06-bouncing-smiley/sketch.js
@@ -9,48 +9,78 @@ let collision = false;
 let gameOver = false;
 
 // styling settings
-const bgColors = ['#a1ffed', '#f9aaaa', '#ddffab'];
-const fillColors = ['#a1c9ff', '#fef7ad', '#abffb4'];
-let colorIteration = 1;
+const BG_COLORS = ['#a1ffed', '#f9aaaa', '#ddffab'];
+const FILL_COLORS = ['#a1c9ff', '#fef7ad', '#abffb4'];
+let colorIteration;
 
 // ball settings
-const ballSpeed = 3;
-let ballX = ballSize / 2;
-let ballY = ballSize / 2;
-let ballDirectionX = 1;
-let ballDirectionY = 1;
+const BALL_SPEED = 3;
+let ballX;
+let ballY;
+let ballDirectionX;
+let ballDirectionY;
 
 // racket settings
 const keySpeed = 10;
 const racketY = canvasSize - racketHeight - 20;
 let racketX = 0;
 
-// text settings
-const gameOverText = 'GAME OVER';
+// game over settings
+const GAME_OVER_TEXT = 'GAME OVER';
+const BUTTON_TEXT = 'Restart';
+const buttonWidth = 100;
+const buttonHeight = 40;
+const buttonX = canvasSize / 2 - buttonWidth / 2;
+const buttonY = canvasSize / 2 - buttonHeight / 2;
+let button;
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function restart() {
+  gameOver = false;
+  button.hide();
+  createNewGame();
+}
+
+function createNewGame() {
+  ballX = randomIntFromInterval(minCanvasPosition, maxCanvasPosition);
+  ballDirectionX = Math.random() < 0.5 ? 1 : -1;
+  ballY = ballSize / 2;
+  ballDirectionX = 1;
+  ballDirectionY = 1;
+  colorIteration = randomIntFromInterval(0, BG_COLORS.length - 1);
+}
+
 function setup() {
   createCanvas(canvasSize, canvasSize);
+  createNewGame();
 
-  ballX = randomIntFromInterval(minCanvasPosition, maxCanvasPosition);
-  ballDirectionX =  Math.random() < 0.5 ? 1 : -1;
-  colorIteration = randomIntFromInterval(0, bgColors.length - 1);
+  // adds restart button
+  button = createButton(BUTTON_TEXT);
+  button.position(buttonX, buttonY);
+  button.mousePressed(restart);
+  button.size(buttonWidth, buttonHeight);
+  button.style('background-color', BG_COLORS[colorIteration]);
+  button.style('border', FILL_COLORS[colorIteration]);
+  button.style('box-shadow', 'none');
+  button.style('color', FILL_COLORS[colorIteration]);
+  button.style('font-family', 'Helvetica');
+  button.style('font-size', 32);
 }
 
 function draw() {
   if (!gameOver) {
-    background(bgColors[colorIteration]);
+    background(BG_COLORS[colorIteration]);
     noStroke();
 
     // create motion
-    ballX = ballX + ballDirectionX * ballSpeed;
-    ballY = ballY + ballDirectionY * ballSpeed;
+    ballX = ballX + ballDirectionX * BALL_SPEED;
+    ballY = ballY + ballDirectionY * BALL_SPEED;
 
     // creates ball
-    fill(fillColors[colorIteration]);
+    fill(FILL_COLORS[colorIteration]);
     ellipse(ballX, ballY, ballSize, ballSize);
 
     // creates racket
@@ -72,7 +102,7 @@ function draw() {
 
     // collision control
     if (collision === true) {
-      colorIteration = (colorIteration + 1) % bgColors.length;
+      colorIteration = (colorIteration + 1) % BG_COLORS.length;
       collision = false;
     }
 
@@ -87,11 +117,14 @@ function draw() {
       racketX = racketX - keySpeed;
     }
   } else {
+    // adds game over text
     textSize(50);
     textAlign(CENTER, CENTER);
     textFont('Helvetica');
     background('#f5f5f5');
     fill(0);
-    text(gameOverText, 0, 0, canvasSize, canvasSize / 3 * 2);
+    text(GAME_OVER_TEXT, 0, 0, canvasSize, canvasSize / 3 * 2);
+
+    button.show();
   }
 }
