47bbac12fac3f25c1fc7659c68df0bb33b6d459e
Jose Sentis <jose.sentis1987@gmail.com>
2021-05-03 16:52:54 +0200

    Moves camera on mouse move

--- 09-3D-text/src/script.js
+++ 09-3D-text/src/script.js
@@ -1,14 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-import { Texture } from 'three'
+import gsap from 'gsap';
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,8 +17,8 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene();
 
 // Axis helper
-// const axesHelper = new THREE.AxesHelper();
-// scene.add(axesHelper);
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Textures
@@ -45,17 +45,8 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
       bevelSegments: 4
     }
   );
-
-  // textGeometry.computeBoundingBox();
-  // console.log(textGeometry.boundingBox);
-
-  // textGeometry.translate(
-  //   - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-  //   - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-  //   - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-  // );
-
   textGeometry.center();
+
   const material = new THREE.MeshMatcapMaterial({
     matcap: matcapTexture
   });
@@ -65,7 +56,7 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
 
   const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
 
-  for (let i = 0; i <= 300; i++) {
+  for (let i = 0; i <= 350; i++) {
     const donut = new THREE.Mesh(donutGeometry, material);
 
     donut.position.x = (Math.random() - 0.5) * 20;
@@ -80,11 +71,20 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
 
     scene.add(donut);
   }
-})
+});
 
 /**
- * Object
+ * Cursor
  */
+const cursor = {
+  x: 0,
+  y: 0
+};
+
+window.addEventListener('mousemove', event => {
+  cursor.x = event.clientX / window.innerWidth - 0.5;
+  cursor.y = - (event.clientY / window.innerHeight - 0.5);
+});
 
 /**
  * Sizes
@@ -92,7 +92,7 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
-}
+};
 
 window.addEventListener('resize', () => {
   // Update sizes
@@ -106,21 +106,21 @@ window.addEventListener('resize', () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 5;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
 /**
  * Renderer
@@ -134,19 +134,34 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+
+const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+  // Update camera on mouse move
+  // camera.position.x = cursor.x * 10;
+  // camera.position.y = cursor.y * 10;
+  // camera.lookAt(0, 0);
+
+  gsap.to(camera.position, {
+    duration: 1,
+    x: cursor.x * 10,
+    y: cursor.y * 10,
+    onUpdate: function () {
+      camera.lookAt(new THREE.Vector3(0, 0, 0));
+    }
+  });
+
+  const elapsedTime = clock.getElapsedTime();
 
   // Update controls
-  controls.update()
+  // controls.update();
 
   // Render
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
+  window.requestAnimationFrame(tick);
 }
 
-tick()
+tick();
